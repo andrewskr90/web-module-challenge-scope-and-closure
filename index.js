5b6582b39fae8cb2beb03f7f866b055b7c91d03d
Kyle Andrews <andrewskr90@gmail.com>
2021-06-14 17:16:22 -0700

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+     Counter1 is a function containing another function, whereas counter2 is one single function.
   2. Which of the two uses a closure? How can you tell?
-  
+    Counter1 uses a closure when count++ is returned. I can well because count is referencing a variable that was declared in a parent function to the function count++ resides in.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     Counter1 would be preferable if you want to keep track of multiple counts, or if you ever want to reset a count. Counter2 would be the better option if you wanted to create a universal counter that could not be reset.
 */
 
 // counter1 code
@@ -62,8 +63,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.floor(Math.random()*3);
 }
 
 
@@ -81,18 +82,32 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, inningNum){
+  let scoreHome = 0;
+  let scoreAway = 0;
+  for(let i=0; i<inningNum; i++){
+    scoreHome = scoreHome + inningCB();
+    scoreAway = scoreAway + inningCB();
+  };
+  return {
+    "Home": scoreHome,
+    "Away": scoreAway
+  };
 }
+console.log('task 3',finalScore(inning,9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {
+    "Home": inningCB(),
+    "Away": inningCB()
+  };
 }
+console.log('task 4',getInningScore(inning));
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
@@ -136,9 +151,16 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScoreCB, inningCB, inningNum) {
+  const allInnings = [];
+  for (let i = 0; i< inningNum; i++){
+    let eachInning = getInningScoreCB(inningCB);
+    allInnings.push(`Inning ${i+1}: ${eachInning}`);
+  }
+
+  return allInnings;
 }
+console.log('task 5', scoreboard(getInningScore,inning,9));
 
 
 
